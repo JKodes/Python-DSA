d2247fb347c9a375df3d3a6154b7c479c2452042
JKodes <adrainford09@gmail.com>
2022-09-02 11:41:37 -0400

    add a folder call binary that stores all binary search problems

---
+++ BinarySearch/704.BinarySearch.py
@@ -0,0 +1,31 @@
+def BinarySearch(nums, target):
+    left = 0
+    right = len(nums)-1
+
+    while(left<=right):
+        mid = (left + right)//2
+
+        if nums[mid] == target:
+            return mid
+        elif nums[mid] < target:
+            left = mid + 1
+        else:
+            right = mid - 1
+
+    return -1
+
+
+
+
+
+
+
+
+
+
+if __name__=='__main__':
+    print(BinarySearch([-1,0,3,5,9,12], 9))
+
+
+#the problem solution correct I've plug in both Example from LeetCode an
+#arrived at the answers.
