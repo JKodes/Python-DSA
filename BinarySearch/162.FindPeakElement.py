283016782f5bc1e1302ee6289ca7519f2d855c1c
JKodes <adrainford09@gmail.com>
2022-09-26 23:00:57 -0400

    created if statement conditon to help find the peak

--- BinarySearch/162.FindPeakElement.py
+++ BinarySearch/162.FindPeakElement.py
@@ -6,7 +6,13 @@ def  FindPeakElement(nums):
     while(left< right):
         mid = (left + right)//2
 
+        if nums[mid] > nums[mid + 1]:
+            right = mid
         
+        else:
+            left = mid + 1
+    
+    return left
 
 
 
