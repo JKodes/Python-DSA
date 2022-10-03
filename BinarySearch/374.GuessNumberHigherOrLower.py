4e7b4ce9ea83a45d871ebae32bf0338b917450c1
JKodes <adrainford09@gmail.com>
2022-10-02 23:54:07 -0400

    fixed layout

--- BinarySearch/374.GuessNumberHigherOrLower.py
+++ BinarySearch/374.GuessNumberHigherOrLower.py
@@ -1,17 +1,17 @@
-    def guessNumber() -> int:
-        left = 1
-        right = n
+def guessNumber() -> int:
+    left = 1
+    right = n
         
-        while left < right:
-            mid = (left + right) // 2
-            res = guess(mid)
+    while left < right:
+        mid = (left + right) // 2
+        res = guess(mid)
             
-            if res > 0:
-                left = mid + 1
-            elif res < 0:
-                right = mid - 1
-            else:
-                return mid
+        if res > 0:
+            left = mid + 1
+        elif res < 0:
+            right = mid - 1
+        else:
+            return mid
 
 
 if __name__ == '__main__':
