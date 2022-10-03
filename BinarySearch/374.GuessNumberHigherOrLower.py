d2d361f0e827b60ad8b265fbf2be93ab7147d916
JKodes <adrainford09@gmail.com>
2022-10-02 23:50:11 -0400

    create add solution for problem

---
+++ BinarySearch/374.GuessNumberHigherOrLower.py
@@ -0,0 +1,14 @@
+    def guessNumber() -> int:
+        left = 1
+        right = n
+        
+        while left < right:
+            mid = (left + right) // 2
+            res = guess(mid)
+            
+            if res > 0:
+                left = mid + 1
+            elif res < 0:
+                right = mid - 1
+            else:
+                return mid
