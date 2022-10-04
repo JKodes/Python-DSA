8ec849db4121b01afcbd0c4978e68a87c7135dbc
JKodes <adrainford09@gmail.com>
2022-10-03 22:34:44 -0400

    create a solution

--- BinarySearch/441.ArrangingCoins.py
+++ BinarySearch/441.ArrangingCoins.py
@@ -1,10 +1,20 @@
-from tkinter import N
-
-
 def arrangingcoins(n):
-    left = 1
-    right =  n
+    left =1
+    right = n
     res = 0
-
+        
+    while left <= right:
+        mid = (left + right) // 2
+        coins = (mid / 2) * (mid + 1)
+        if coins > n:
+            right = mid - 1
+        else:
+            left = mid + 1
+            res = max(mid, res)
+    return res
     
+
+if __name__ == '__main__':
+    print(arrangingcoins(4))
+
     
