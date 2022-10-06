69471f095c9ac45317fae7ab6feca7075c49d601
JKodes <adrainford09@gmail.com>
2022-10-05 22:47:27 -0400

    add standard binary search until tweak for overall purposes

--- BinarySearch/34.FindFirstAndLastPostionOfElementInSortedArray.py
+++ BinarySearch/34.FindFirstAndLastPostionOfElementInSortedArray.py
@@ -1,3 +1,12 @@
 def DoubleBinarySearch(self, nums, target, left_most):
     left = 0
     right = len(nums)-1
+    i = -1
+    while left <= right:
+        mid = (left + right) // 2
+
+        if nums[mid] > target:
+            right = mid -1
+        elif nums[mid] < target:
+            left = mid + 1
+        
