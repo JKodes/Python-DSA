c4f78430fcecbc585ab60f2f9126b5bbf5571be9
JKodes <adrainford09@gmail.com>
2022-10-05 23:10:42 -0400

    print out two examples results from the promblem to show it works

--- BinarySearch/34.FindFirstAndLastPostionOfElementInSortedArray.py
+++ BinarySearch/34.FindFirstAndLastPostionOfElementInSortedArray.py
@@ -1,4 +1,6 @@
-def DoubleBinarySearch(self, nums, target, left_most):
+
+
+def DoubleBinarySearch(nums, target, left_most):
     left = 0
     right = len(nums)-1
     i = -1
@@ -15,9 +17,16 @@ def DoubleBinarySearch(self, nums, target, left_most):
                 right = mid - 1
             else:
                 left = mid + 1
-
+    return i
     
 
 
-
-        
+if __name__ == '__main__':
+    left = DoubleBinarySearch([5,7,7,8,8,10], 8, True)
+    right = DoubleBinarySearch([5,7,7,8,8,10], 8, False)
+    print('Example 1:')
+    print(left, right)
+    left = DoubleBinarySearch([5,7,7,8,8,10], 6, True)
+    right = DoubleBinarySearch([5,7,7,8,8,10], 6, False)
+    print('Example 2:')
+    print(left, right)
