e94729593a8fc5cd1265e3418cadf9572f196f51
JKodes <adrainford09@gmail.com>
2022-10-06 23:58:16 -0400

    implement the standarad binary search

--- BinarySearch/744.FindSmallestLetterGreaterThanTarget.py
+++ BinarySearch/744.FindSmallestLetterGreaterThanTarget.py
@@ -2,3 +2,12 @@ def FindSmallestLetterGreaterThanTarget(letters):
     left = 0
     right = len(letters)
 
+
+    while left < right:
+        mid = (left + right) // 2
+
+        if letters[mid] > target:
+            right = mid - 1
+        else:
+            left = mid + 1
+
