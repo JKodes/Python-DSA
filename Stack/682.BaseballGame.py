b6e2e655d5f51d41b2cbcf9fc0d12b8d8651f515
JKodes <adrainford09@gmail.com>
2022-10-12 23:05:37 -0400

    output gave me results from example

--- Stack/682.BaseballGame.py
+++ Stack/682.BaseballGame.py
@@ -2,7 +2,18 @@ def BaseballGame(ops):
     stack = []
 
     for op in ops:
+        if op == "+":
+            stack.append(stack[-1] + stack[-2])
+        elif op == "D":
+            stack.append(stack[-1] + stack[-1])
+        elif op == "C":
+            stack.pop()
+        else:
+            stack.append(int(op))
+    return sum(stack)
 
+if __name__ == '__main__':
+    print(BaseballGame(["5","2","C","D","+"]))
             
 
 
