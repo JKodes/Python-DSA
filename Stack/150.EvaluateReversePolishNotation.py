0b3d289a3521f8e5e6050ba883c45c1db18ca339
JKodes <adrainford09@gmail.com>
2022-10-13 23:20:46 -0400

    add some conditions

--- Stack/150.EvaluateReversePolishNotation.py
+++ Stack/150.EvaluateReversePolishNotation.py
@@ -2,4 +2,16 @@ def EvaluateReversePolishNotation(tokens):
     stack = []
 
     for token in tokens:
-        
+        if token  == "+":
+            stack.append(stack.pop() + stack.op())
+        elif token == "-":
+            x, y = stack.pop() , stack.pop()
+            stack.append(y - x)
+        elif token == "*":
+            stack.append(stack.pop() * stack.op())
+        elif token == "/":
+            x, y = stack.pop() , stack.pop()
+            stack.append(int(y / x))
+    return stack[0]
+
+
