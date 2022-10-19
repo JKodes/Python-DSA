150951b16608d9fe2dcf7d607b90150a515a78d0
JKodes <adrainford09@gmail.com>
2022-10-18 23:26:29 -0400

    create binary search to find non duplicate value in the array

--- BinarySearch/540.SingleElementInSortedArray.py
+++ BinarySearch/540.SingleElementInSortedArray.py
@@ -1,3 +1,13 @@
 def singleNonDuplicate(nums):
     left = 0
     right = len(nums)-1
+
+    while left < right:
+        mid = 2* ((left + right)// 4)
+
+        if nums[mid] == nums[mid +1]:
+            left = mid + 1
+        else:
+            right = mid 
+    
+    return nums[left]
