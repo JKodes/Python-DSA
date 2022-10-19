d35d9ea0980091fec710db5d7c6719c70540919c
JKodes <adrainford09@gmail.com>
2022-10-18 23:45:19 -0400

    create a function that looks through the array

--- BinarySearch/540.SingleElementInSortedArray.py
+++ BinarySearch/540.SingleElementInSortedArray.py
@@ -6,8 +6,13 @@ def singleNonDuplicate(nums):
         mid = 2* ((left + right)// 4)
 
         if nums[mid] == nums[mid +1]:
-            left = mid + 1
+            left = mid + 2
         else:
             right = mid 
     
     return nums[left]
+
+
+
+if __name__ == '__main__':
+    print(singleNonDuplicate([1,1,2,3,3,4,4,8,8]))
