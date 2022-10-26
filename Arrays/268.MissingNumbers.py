d73ee6f9dc6ff8770116d52c3dc128765a49666c
JKodes <adrainford09@gmail.com>
2022-10-26 00:27:30 -0400

    my solution works

--- Arrays/268.MissingNumbers.py
+++ Arrays/268.MissingNumbers.py
@@ -1,1 +1,12 @@
-def missingNumber(num):
+def missingNumber(nums):
+
+    n = len(nums)+ 1
+    missingsum = (n/2)*(n-1)
+
+    return missingsum - sum(nums)
+
+
+
+
+if '__name__' == '__main__':
+    print(missingNumber([3,0,1]))
