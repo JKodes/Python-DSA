48c07ecb1df8b1e8046e2489ff32f3ec2975c8d1
JKodes <adrainford09@gmail.com>
2022-10-30 00:20:50 -0400

    created a valid solution

--- Arrays/TwoPointers/680.VaildPalindromeII.py
+++ Arrays/TwoPointers/680.VaildPalindromeII.py
@@ -4,4 +4,28 @@ def validPalindrome(s):
 
     while left < right:
         if s[left] != s[right]:
-            return is_palidin(i, )
+            return self.is_palidin(s, left + 1, right) or self.is_pladin(s, left, right - 1)
+
+        else:
+            left += 1
+            right -= 1
+
+    return True
+
+def is_pladin(self, s, right, left):
+    left = 0
+    right = len(s)-1
+
+    while left < right:
+        if s[left] != s[right]:
+            return False
+        else:
+            left +=1
+            right +=1
+
+    return True
+
+
+
+if __name__ == '__main__':
+    print(validPalindrome("abca"))
