48502f8838b46ae67e68bf076ffc447b56212aba
JKodes <adrainford09@gmail.com>
2022-10-30 22:43:11 -0400

    add the nums array first time solution ran i got one relaize I miss th equal sign to left to go up by 1

--- Arrays/TwoPointers/26.RemoveDuplicatesFromSortedArray.py
+++ Arrays/TwoPointers/26.RemoveDuplicatesFromSortedArray.py
@@ -5,5 +5,10 @@ def RemoveDuplicates(nums):
     for right in range(1, len(nums)):
         if nums[right] != nums[right - 1]:
             nums[left] = nums[right]
-            left + 1
+            left += 1
     return left
+
+
+
+if __name__ == '__main__':
+    print(RemoveDuplicates([0,0,1,1,1,2,2,3,3,4]))
