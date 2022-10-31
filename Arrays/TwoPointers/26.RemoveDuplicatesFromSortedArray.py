1a2f2324fb2933b7c772383db52dcd2688038ef7
JKodes <adrainford09@gmail.com>
2022-10-30 22:37:52 -0400

    in the condition we used compared the right pointers with its previous once its not the same value its sent to the left pointer

--- Arrays/TwoPointers/26.RemoveDuplicatesFromSortedArray.py
+++ Arrays/TwoPointers/26.RemoveDuplicatesFromSortedArray.py
@@ -3,4 +3,7 @@ def RemoveDuplicates(nums):
     left = 1
 
     for right in range(1, len(nums)):
-        
+        if nums[right] != nums[right - 1]:
+            nums[left] = nums[right]
+            left + 1
+    return left
