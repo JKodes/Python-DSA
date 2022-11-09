320f3bd885f63b9b3e8169b652498a998e851867
JKodes <adrainford09@gmail.com>
2022-11-08 22:53:26 -0500

    called a function that will help pick the right iterate through the weights

--- BinarySearch/1011.CapacityToShipPackagesWithinDDays.py
+++ BinarySearch/1011.CapacityToShipPackagesWithinDDays.py
@@ -6,5 +6,11 @@ def DaysToShip(weights, days):
     while left < right:
         mid = (left + right) // 2
 
+        if self.package_ships(mid, weights, days):
+            right = mid
+        else:
+            left = mid + 1
+
+
 
     
