8673d518333a59ec876acd1145c212cc2510cc3c
JKodes <adrainford09@gmail.com>
2022-11-08 23:04:15 -0500

    create functon that would help us iterate weights and find the minimum day

--- BinarySearch/1011.CapacityToShipPackagesWithinDDays.py
+++ BinarySearch/1011.CapacityToShipPackagesWithinDDays.py
@@ -6,11 +6,27 @@ def DaysToShip(weights, days):
     while left < right:
         mid = (left + right) // 2
 
-        if self.package_ships(mid, weights, days):
+        if package_ships(mid, weights, days):
             right = mid
         else:
             left = mid + 1
 
+    return right
+
+def package_ships(candidate, weights, days):
+    current_weight = 0
+    days_taken = 1
+
+    for weight in weights:
+        current_weight += weight
+
+        if current_weight > candidate:
+            days_taken += 1
+            current_weight = weight
+
+    return days_taken <= days
 
 
-    
+
+if __name__ == '__main__':
+    print(DaysToShip([1,2,3,4,5,6,7,8,9,10], 5))
