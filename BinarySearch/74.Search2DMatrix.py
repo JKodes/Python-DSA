6a3f8c9995f6e83331438a381808364adcd5736a
JKodes <adrainford09@gmail.com>
2022-11-22 00:06:18 -0500

    binry search for rows

--- BinarySearch/74.Search2DMatrix.py
+++ BinarySearch/74.Search2DMatrix.py
@@ -6,6 +6,23 @@ def searchMatrix(matrix, target):
     top_row = 0
     bottom_row = ROWS - 1
 
+    while top_row <= bottom_row:
+        row = (top_row + bottom_row) // 2
+
+        if target > matrix[row][-1]:
+            top_row = row + 1
+        elif target < matrix[row][0]:
+            bottom_row = row - 1
+        else:
+            break
+
+    if not(top_row <= bottom_row):
+        return False
+    
+
+
+
+
 
 
 
