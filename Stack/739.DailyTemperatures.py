4aa5a14c03e90f7a5fa9708790e009d3f9017c7f
JKodes <adrainford09@gmail.com>
2023-01-02 23:59:37 -0500

    final solution

--- Stack/739.DailyTemperatures.py
+++ Stack/739.DailyTemperatures.py
@@ -1,21 +1,17 @@
 def DailyTemperatures(temperatures):
     stack = []
-    res = [0]  * len(temperatures)
+    res = [0] * len(temperatures)
 
 
+    for cur_day, cur_temp in enumerate(temperatures):
+        while stack and cur_temp > stack[-1][1]:
+            prev_day, prev_temp = stack.pop()
 
+            res[prev_day] = cur_day - prev_day
+            
+            stack.append((cur_day, cur_temp))
 
-
-
-
-
-
-
-
-
-
-
-
+    return res
 
 
 
