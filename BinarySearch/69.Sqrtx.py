6d954d3030287ffd1ff83d2e3ead3d2ef666f7f4
JKodes <adrainford09@gmail.com>
2023-01-25 23:01:14 -0500

    start binary search approach

---
+++ BinarySearch/69.Sqrtx.py
@@ -0,0 +1,28 @@
+def sqrt(x):
+    if x == 0 and x == 1:
+        return x 
+    
+    left = 1
+    right = x
+
+    while left <= right:
+        mid =(left + right) // 2
+
+        
+
+        
+    
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    print(sqrt(4))
