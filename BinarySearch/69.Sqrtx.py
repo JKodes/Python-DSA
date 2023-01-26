d0e96af864f6f226ca647cd0c120f42790621958
JKodes <adrainford09@gmail.com>
2023-01-25 23:24:51 -0500

    solution is approved answer solved

--- BinarySearch/69.Sqrtx.py
+++ BinarySearch/69.Sqrtx.py
@@ -8,21 +8,27 @@ def sqrt(x):
     while left <= right:
         mid =(left + right) // 2
 
-        
+        square = mid * mid
+
+        if square == x:
+            return mid 
+        elif square < x:
+            left = mid + 1
+            value = mid 
+        else:
+            right = mid - 1
+    return value 
 
         
-    
-
-
-
-
-
-
-
+    '''
+        [1,2,3,4,5,6,7,8,9,10,11,12,13]
+               L R                   X
+               M 
+    '''
 
 
 
 
 
 if __name__ == '__main__':
-    print(sqrt(4))
+    print(sqrt(13))
